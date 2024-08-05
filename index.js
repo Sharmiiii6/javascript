7df593c34117fe2a68f8b9c8a153e95a4cf1e7a9
Sharmiiii6 <sharmiadhikary3@gmail.com>
2024-08-05 17:29:49 +0530

    type and variables

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
-const a=10;
-const b=true;
-console.log(b);
-
-function test(){
+console.log("hello from external javascript");
+// /**
+//  * Example 1- Variable declaretion
+//  */
+var a;
 console.log(a);
-console.log(b)
+let c;
+console.log(c);
+
+/**
+ * Example 2- "let"
+ */
+ let myNumber=10;
+console.log(myNumber);
+
+let myString;//declaration
+myString="Hello from string";//assignment
+console.log(myString);
+
+myString= "new value for string";//reassignment
+console.log(myString);
+
+/**
+ * Examole 3-"const"
+ */
+const myBoolean=false;
+console.log(myBoolean);
+
+/**
+ * Challenges 1
+ */
+
+let myObject= {}
+console.log(myObject);
+
+/**
+ * Challenges 1
+ */
+let x= 10
+let y= true
+const myObject1={
+    a:x,
+    b:y
 }
+console.log(myObject1);
+
+x=20;
+let anotherObject;
+anotherObject={
+    newA:x,
+    b:y,
+    c:myObject1
+}
+console.log(anotherObject);
+
+
+newVariable=10;
+console.log(newVariable);
+
+newVariable=true;
+console.log(newVariable);
+
+newVariable={
+    x:true,
+    y:10
+};
+console.log(newVariable);
+
+newVariable.y=20;
+console.log(newVariable);
+
+//this will break the code
+//newVariable=null;
+newVariable.x=false;
+console.log(newVariable);
+
+
